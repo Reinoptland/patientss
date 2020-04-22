0235c2094eb814d6e86bcf423a09b34f602742da
Reinoptland <reinoptland@gmail.com>
2020-04-22 16:30:54 +0200

    Make plan

---
+++ filterByWeight.js
@@ -0,0 +1,9 @@
+// input
+// weight: number
+// patients: array of objects
+
+// output: patients (array of objects)
+// but only people who weight less then the input weight
+
+// processing?
+// array.filter -> [{}, {}, {}, {}] -> [{}, {}]
