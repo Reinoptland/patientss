ce8ddf37fc2dac2df3fc1fd106239080b1c2f0d4
Reinoptland <reinoptland@gmail.com>
2020-04-22 16:19:58 +0200

    Write logic and test the program

--- findByPhoneNumber.js
+++ findByPhoneNumber.js
@@ -17,6 +17,18 @@ console.log(patients);
 
 const inputPhoneNumber = "+55-656-460-8983";
 
-const patientWeAreLookingFor = patients.find(() => {
-  console.log("hi");
+const patientWeAreLookingFor = patients.find((patient) => {
+  console.log("number:", patient.phoneNumber);
+  console.log("correct?", inputPhoneNumber === patient.phoneNumber);
+  if (inputPhoneNumber === patient.phoneNumber) {
+    return true;
+  } else {
+    return false;
+  }
 });
+
+console.log(patientWeAreLookingFor);
+
+// predictions:
+// 1 output, number, correct true
+// list of numbers (18): number, correct false / true, the patient we are looking for
