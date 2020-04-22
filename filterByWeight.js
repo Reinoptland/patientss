810f8d53711538489be6441b9a6ee379a965ec9e
Reinoptland <reinoptland@gmail.com>
2020-04-22 16:48:01 +0200

    Filter example

--- filterByWeight.js
+++ filterByWeight.js
@@ -1,3 +1,23 @@
+const patients = require("./data");
+// console.log(patients);
+const weight = parseInt(process.argv[2]);
+
+const output = patients.filter((patient) => {
+  console.log("weight", patient.weight);
+  return patient.weight <= weight;
+});
+
+console.log(output);
+
+// syntax error (red underline)
+// do we have the data, the patients
+// weight -> not correct datatype
+// made syntax while writing my functions
+// I did not check what a patient object looks like, what data it has
+// no check on my logic
+
+// seperate topic semis or no semis
+
 // input
 // weight: number
 // patients: array of objects
