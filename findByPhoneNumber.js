af7a3b09ae765634d27a633e1a406a13c889ea8a
Reinoptland <reinoptland@gmail.com>
2020-04-22 16:08:21 +0200

    Make a plan

--- findByPhoneNumber.js
+++ findByPhoneNumber.js
@@ -1,3 +1,16 @@
 const patients = require("./data");
 
 console.log(patients);
+
+// assumption: phone number is unique
+// input phonennumber
+// output patient
+
+// data model
+// patients: array of objects
+// inputPhoneNumber: string
+
+// processing?
+// loop?
+// array filter? [{}, {}, {}] -> [{}, {}]
+// array find? [{}, {}, {}] -> {}
